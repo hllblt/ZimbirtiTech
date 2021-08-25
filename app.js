c869a18d311340a8d081c84772e759a924098c67
Halil <halill.bulutt.1@gmail.com>
2021-08-26 01:40:41 +0200

    set cart list

--- app.js
+++ app.js
@@ -25,15 +25,18 @@ const productDescs = [
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
 ];
 
+let arrayID = [];
+let productID;
 
 const choose = function (id) {
   let newIndex = Number(id.slice(7));
+  productID = id;
 
   //aside visibility
   let aside = document.querySelector("aside");
   aside.style.display = "block";
 
-  // assigning product img to aside img 
+  // assigning product img to aside img
   let asideImg = document.querySelector(`.productDescription img`);
   let productImg = document.querySelector(`#${id} img`).src;
   asideImg.src = productImg;
@@ -52,26 +55,103 @@ const choose = function (id) {
   let asideDescription = document.querySelectorAll(`.productDescription>p`)[2];
   asideDescription.innerHTML = productDescs[newIndex];
 
-  //aside quantity 
+  //aside quantity
   let quantity = document.querySelector(`.productDescription input`);
   quantity.value = 1;
 
   //aside total
   let total = document.querySelectorAll(`.productDescription p`)[4];
   total.innerHTML = quantity.value * Number(productPrice.slice(0, -1)) + "₺";
-}
+};
+
 //calculating totalprice
 const calcTotal = function () {
   let quantity = document.querySelector(`.productDescription input`);
   let total = document.querySelectorAll(`.productDescription p`)[4];
-  let asidePrice = document.querySelectorAll(`.productDescription>p`)[1].innerHTML;
+  let asidePrice = document.querySelectorAll(`.productDescription>p`)[1]
+    .innerHTML;
   total.innerHTML = quantity.value * Number(asidePrice.slice(0, -1)) + "₺";
-
-}
+};
 //aside bar closer
 const close = function () {
   let aside = document.querySelector("aside");
   aside.style.display = "none";
-}
+};
 const closerButton = document.getElementById("closer");
 closerButton.addEventListener("click", close);
+
+//to add products to cart list
+const addToCart = () => {
+  let asideImg = document.querySelector(`.productDescription img`).src;
+  let asideTitle = document.querySelectorAll(`.productDescription>p`)[0]
+    .innerHTML;
+  let asidePrice = document.querySelectorAll(`.productDescription>p`)[1]
+    .innerHTML;
+  let quantity = document.querySelector(`.productDescription input`).value;
+
+  if (arrayID.includes(`Cart${productID}`)) {
+    let cartQuantity = document.querySelector(`#Cart${productID} input`);
+    cartQuantity.value = Number(cartQuantity.value) + Number(quantity);
+    let cartLi = document.querySelectorAll(`#Cart${productID} li`);
+    cartLi[4].innerHTML = (cartQuantity.value*Number(cartLi[2].innerHTML.slice(0,-1))).toFixed(2) + "₺";
+  }
+
+
+
+  // Number((6.688689).toFixed(1)); // 6.7
+
+
+
+
+  else {
+    //creating new ul for new item to cart list
+    let newItem = document.createElement("ul");
+    newItem.className = "productCartList";
+    newItem.id = `Cart${productID}`;
+
+    //creating li*6 for new item's ul
+    let arrayLi = [];
+    for (let i = 0; i < 6; i++) {
+      let newLi = document.createElement("li");
+      arrayLi.push(newLi);
+    }
+
+    //creating and assinging image information
+    let newImg = document.createElement("img");
+    newImg.src = asideImg;
+    arrayLi[0].appendChild(newImg);
+
+    //assinging title information
+    arrayLi[1].innerHTML = asideTitle;
+    arrayLi[1].className = "cartTitle";
+
+    //assinging price information
+    arrayLi[2].innerHTML = asidePrice;
+
+    //creating and assinging quantity information
+    let newInput = document.createElement("input");
+    newInput.value = quantity;
+    arrayLi[3].appendChild(newInput);
+
+    //calculating and assigning total price information
+    arrayLi[4].innerHTML = (newInput.value*Number(arrayLi[2].innerHTML.slice(0,-1))).toFixed(2) + "₺";
+    // console.log(arrayLi[3]);
+    
+    //creating remove button
+    let newRemove = document.createElement("button");
+    newRemove.innerHTML = "X";
+    arrayLi[5].appendChild(newRemove);
+
+    //appending lis to ul
+    for(let i = 0;i<6;i++){
+      newItem.appendChild(arrayLi[i]);
+    }
+
+    //appending new ul to cart list
+    document.getElementsByClassName("cart")[0].appendChild(newItem);
+
+    // adding id of new item to array to check if it exists in the list
+    arrayID.push(newItem.id);
+  }
+  
+};
