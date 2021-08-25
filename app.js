44e6ff39897102ce4b599dc32109e86118270d1e
Halil <halill.bulutt.1@gmail.com>
2021-08-25 13:56:04 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const productDescs = [
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste exercitationem illum delectus assumenda aliquam illo reprehenderit atque accusantium numquam, corrupti molestiae temporibus nostrum aperiam explicabo laudantium tenetur rem quae amet minus beatae? Iure odio minima unde obcaecati. Animi earum, nulla impedit assumenda aut itaque doloribus ab nobis vitae, provident qui?",
+];
+
+
+const choose = function(id){
+  let newIndex = Number(id.slice(7));
+
+  // assigning product img to aside img 
+  let asideImg = document.querySelector(`.productDescription img`);
+  let productImg = document.querySelector(`#${id} img`).src;
+  asideImg.src = productImg;
+
+  //assigning product title to aside title
+  let asideTitle = document.querySelectorAll(`.productDescription>p`)[0];
+  let productTitle = document.querySelectorAll(`#${id} p`)[0].innerHTML;
+  asideTitle.innerHTML = productTitle;
+
+  //assigning product price to aside price
+  let asidePrice = document.querySelectorAll(`.productDescription>p`)[1];
+  let productPrice = document.querySelectorAll(`#${id} p`)[1].innerHTML;
+  asidePrice.innerHTML = productPrice
+
+  //assigning product description to aside description
+  let asideDescription = document.querySelectorAll(`.productDescription>p`)[2];
+  asideDescription.innerHTML = productDescs[newIndex];
+
+  //
+
+
+}
